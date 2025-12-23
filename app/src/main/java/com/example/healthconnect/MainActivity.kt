e29fff38d41c620e4ad308e4f52f66efe9293b44
owlN-21 <nastakharina21@gmail.com>
2025-12-23 12:28:09 +0300

    Request permissions from the user

--- app/src/main/java/com/example/healthconnect/MainActivity.kt
+++ app/src/main/java/com/example/healthconnect/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -15,25 +16,70 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.health.connect.client.HealthConnectClient
 import androidx.health.connect.client.HealthConnectFeatures
+import androidx.health.connect.client.PermissionController
+import androidx.health.connect.client.permission.HealthPermission
+import androidx.health.connect.client.records.HeartRateRecord
+import androidx.health.connect.client.records.StepsRecord
+import androidx.lifecycle.lifecycleScope
 import com.example.healthconnect.ui.theme.HealthConnectTheme
+import kotlinx.coroutines.launch
 
 private const val HEALTH_CONNECT_PACKAGE =
     "com.google.android.apps.healthdata"
 
+private val HEALTH_CONNECT_PERMISSIONS = setOf(
+    HealthPermission.getReadPermission(HeartRateRecord::class),
+    HealthPermission.getWritePermission(HeartRateRecord::class),
+    HealthPermission.getReadPermission(StepsRecord::class),
+    HealthPermission.getWritePermission(StepsRecord::class),
+)
+
 class MainActivity : ComponentActivity() {
+
+
+    private lateinit var requestPermissionsLauncher:
+            ActivityResultLauncher<Set<String>>
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Получаем HealthConnectClient или выходим
+
+        // Launcher для запроса permissions
+        requestPermissionsLauncher =
+            registerForActivityResult(
+                PermissionController.createRequestPermissionResultContract()
+            ) { granted ->
+                if (granted.containsAll(HEALTH_CONNECT_PERMISSIONS)) {
+                    // Permissions successfully granted
+                } else {
+                    // Lack of required permissions
+                }
+            }
+
+        // Получаем HealthConnectClient
         val healthConnectClient = getHealthConnectClient() ?: return
+
+        lifecycleScope.launch {
+            val grantedPermissions =
+                healthConnectClient.permissionController.getGrantedPermissions()
+
+            if (!grantedPermissions.containsAll(HEALTH_CONNECT_PERMISSIONS)) {
+                // Запрашиваем permissions
+                requestPermissionsLauncher.launch(HEALTH_CONNECT_PERMISSIONS)
+            }
+        }
+
 
         // Проверяем доступность фичи background read
         val isBackgroundReadAvailable =
             healthConnectClient.features.getFeatureStatus(
                 HealthConnectFeatures.FEATURE_READ_HEALTH_DATA_IN_BACKGROUND
             ) == HealthConnectFeatures.FEATURE_STATUS_AVAILABLE
-        
+
+
+
         enableEdgeToEdge()
         setContent {
             HealthConnectTheme {
@@ -50,6 +96,9 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+
+
 
     /**
      * Проверяет доступность Health Connect и
